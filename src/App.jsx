e46db0ec9085958c570984c9a2ad9211ae4ba101
Mariano <marianocapiliku@gmail.com>
2023-12-15 19:26:39 +0100

    Build the page's User Iterfaces

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home";
 import { Pricing } from "./pages/Pricing";
 import { PageNotFound } from "./pages/PageNotFound";
 import { AppLayout } from "./pages/AppLayout";
+import { Login } from "./pages/Login";
 
 export function App() {
   return (
@@ -11,10 +12,10 @@ export function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-
+          <Route path="/product" element={<Product />} />
+          <Route path="/pricing" element={<Pricing />} />
           <Route path="/app" element={<AppLayout />} />
+          <Route path="/login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
