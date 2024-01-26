8f6389554b252c88bbb54f9d894cddbb9b236948
Mariano <marianocapiliku@gmail.com>
2024-01-26 13:33:54 +0100

    Implementing tge message component Show a messege to user when the list of cities is empty.

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,9 +1,14 @@
 import styles from "./CityList.module.css";
 import Spinner from "../Spinner/Spinner";
 import CityItem from "../CityItem/CityItem";
+import Message from "../Message/Message";
 
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by cllicking on a city on the map" />
+    );
   return (
     <>
       <ul className={styles.cityList}>
