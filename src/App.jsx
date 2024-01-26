9dd2991059dd3910b0c163e2b8a82009ccba5c93
Mariano <marianocapiliku@gmail.com>
2024-01-26 13:24:03 +0100

    Implementing the city List of cities components Fecthing date from a fake API usinf json-server Create CityItem component to be shown in the city list

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,33 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:9000";
 
 export function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fecthCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+        // setCities(data);
+      } catch {
+        alert("There was an error loading the data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fecthCities();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
@@ -16,8 +41,14 @@ export function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="app" element={<AppLayout />}>
             {/* Nested Routes */}
-            <Route index element={<p>List of cities</p>} />
-            <Route path="cities" element={<p>List of cities</p>} />
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
             <Route path="countries" element={<p>List of countries</p>} />
             <Route path="form" element={<p> Form</p>} />
             {/* ================ */}
